9be65d21f7bdaa97ae93cac9162baecee433153a
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-01 03:53:02 -0400

    passes tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    if len(items) == 2:
+        return ' and '.join(items)
+    if len(items) >= 3:
+        last = items.pop()
+        first = ', '.join(items)
+        return first + ', and ' + last
